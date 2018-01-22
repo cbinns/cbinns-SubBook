b18cf82129780c3ae4e6a08b37ef81619db89893
cbinns <cbinns@ualberta.ca>
2018-01-22 14:17:51 -0700

    quiz 14 code practicing java classes and constructors

---
+++ Quizzes/quiz14.java
@@ -0,0 +1,36 @@
+class A {
+    public A( int initValue ) {
+        System.out.println( "1" );
+    }
+    public A() {
+        System.out.println( "2" );
+    }
+}
+
+class B extends A {
+    protected int value;
+    public B( int initValue ) {
+        System.out.println( "3" );
+        value = initValue;
+    }
+    public B() {
+        this( 1 );
+        System.out.println( "4" );
+    }
+}
+
+class C extends B {
+    public C( int initValue ) {
+        super( initValue );
+        System.out.println( "5" );
+    }
+    public C() {
+        System.out.println( "6" );
+    }
+}
+
+public class Constructors {
+    public static void main( String[] args) {
+        C c = new C();
+    }
+}
